54d125ff263579eba5d37ba26300a07ee1316b23
Shayan Shiravani <shayan5420@yahoo.com>
2022-10-08 13:50:05 +0330

    fix: Refactor code

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express, { Express } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import gateway from './gateway/index'
+import { connectDB } from './db/global'
 
 const app: Express = express()
+connectDB()
 
 app.use(cors({ origin: true, credentials: true }))
 app.use(bodyParser.urlencoded({ extended: false }))
