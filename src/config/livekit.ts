5b7e94cd05a72b8fad9b8b1a40a4bfd57822848e
Shayan Shiravani <shayan5420@yahoo.com>
2022-10-08 13:52:15 +0330

    fix: Refactor code

--- src/config/livekit.ts
+++ src/config/livekit.ts
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export default {
-  host: process.env.LIVEKIT_HOST
+const host = process.env.LIVEKIT_HOST || "ws://localhost:7880"
+const apiKey = process.env.LIVEKIT_API_KEY || "devkey"
+const apiSecret = process.env.LIVEKIT_API_SECRET || "secret"
+
+export {
+  host,
+  apiKey,
+  apiSecret
 }
