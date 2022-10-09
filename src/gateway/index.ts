aec9ab8faea7e1ed06ef1242d44a624147e9188c
Shayan Shiravani <shayan5420@yahoo.com>
2022-10-09 14:24:13 +0330

    feat: Complete functionality

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -1,178 +1,345 @@
 import express, { Router, Request, Response } from 'express'
-import { AccessToken, RoomServiceClient, Room } from 'livekit-server-sdk'
-import livekitConfig from '../config/livekit'
+import { AccessToken, WebhookReceiver } from 'livekit-server-sdk'
+import { apiKey, apiSecret } from '../config/livekit'
+import { body, validationResult } from 'express-validator'
+import { WebhookEvent } from 'livekit-server-sdk/dist/proto/livekit_webhook'
+// import db from "../db/models"
+// import { create as createRoom } from "../db/models/Room"
+// import { create as createParticipant } from "../db/models/Participant"
+import { changeParticipantStatus, createRoom, getAllRooms, getParticipant, getRoom, getRoomParticipants, setParticipantNo, setRoomTurn } 
+from '../utils/livekit'
+import { getCurrentTimestamp } from '../utils/common'
 
 const router: Router = express.Router()
-const livekitHost = livekitConfig.host||"ws://localhost:7880";
+const receiver = new WebhookReceiver(apiKey, apiSecret)
 
 router.get('/', async (req: Request, res: Response, next): Promise<void> => {
-  const svc = new RoomServiceClient(livekitHost);
-  svc.listRooms().then((rooms: Room[]) => {
+
+  let result = await getAllRooms()
+  if(result) {
     res.json({
       success: true,
-      data: rooms
+      data: result
     })
-  }).catch(error => {
+  } else {
     res.json({
       success: false,
-      message: error.message?error.message:error
+      message: "LIVEKIT_ERROR"
     })
+  }
+
+})
+
+router.post('/join', 
+  body('room').isString().not().isEmpty().trim().escape(),
+  body('participant').isString().not().isEmpty().trim().escape(),
+  async (req: Request, res: Response, next): Promise<void> => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      res.json({
+        success: false,
+        message: "INVALID_PARAMS"
+      })
+      return next()
+    }
+
+    const {room: roomName, participant: participantName}: 
+    { room: string, participant: string } = req.body
+
+    try {
+
+      // let room = await db.Room.
+      //   findOne({ name: roomName })
+      //   .populate({
+      //     path: 'participants', 
+      //     match: { username: participantName }
+      //   })
+      //   .exec()
+
+      // if(!room) {
+      //   res.json({
+      //     success: false,
+      //     message: "INVALID_ROOM"
+      //   })
+      //   return next()
+      // }
+
+      let room = await getRoom(roomName)
+      if(!room) {
+        res.json({
+          success: false,
+          message: "INVALID_ROOM"
+        })
+        return next()
+      }
+      
+      if(await getParticipant(roomName, participantName)) {
+        res.json({
+          success: false,
+          message: "DUPLICATE_USERNAME"
+        })
+        return next()
+      }
+
+      const at = new AccessToken(undefined, undefined, {
+        identity: participantName,
+      })
+      at.addGrant({ roomJoin: true, room: roomName });
+      const token = at.toJwt()
+
+      // if(at) {
+      //   let participant = await createParticipant({
+      //     username: participantName,
+      //     room: room._id
+      //   })
+
+      //   room.participants.push(participant)
+      //   room.save()
+        
+      //   res.json({
+      //     success: true,
+      //     data: token
+      //   })
+      // }
+      res.json({
+        success: true,
+        data: token
+      })
+
+    } catch(err: any) {
+      let message = err.message?err.message:err
+      res.json({
+        success: false,
+        message: message
+      })
+    }
+})
+
+router.post(
+  '/create', 
+  body('room').isString().not().isEmpty().trim().escape(), 
+  async (req: Request, res: Response, next): Promise<void> => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      res.json({
+        success: false,
+        message: "INVALID_PARAMS"
+      })
+      return next()
+    }
+
+    const {room: name}: { room: string } = req.body
+
+    try {
+      // if(await db.Room.exists({ name: name })) {
+      //   res.json({
+      //     success: false,
+      //     message: "DUPLICATE_NAME"
+      //   })
+      //   return next()
+      // }
+      if(await getRoom(name)) {
+        res.json({
+          success: false,
+          message: "DUPLICATE_NAME"
+        })
+        return next()
+      }
+
+      let room = await createRoom(name)
+
+      if(room) {
+        // await createRoom({
+        //   sid: room.sid,
+        //   name: room.name,
+        //   maxParticipants: room.maxParticipants,
+        //   creationTime: new Date(room.creationTime * 1000)
+        // })
+        res.json({
+          success: true,
+          data: room
+        })
+        return next()
+      }
+
+    } catch(err: any) {
+      let message = err.message?err.message:err
+      res.json({
+        success: false,
+        message: message
+      })
+      return next()
+    }
+})
+
+router.post('/change-turn', 
+  body('room').isString().not().isEmpty().trim().escape(),
+  async (req: Request, res: Response, next): Promise<void> => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      res.json({
+        success: false,
+        message: "INVALID_PARAMS"
+      })
+      return next()
+    }
+
+    const {room: roomName}: { room: string } = req.body
+
+    try {
+      let room = await getRoom(roomName)
+      if(!room || room.numParticipants < 1) {
+        res.json({
+          success: false,
+          message: "INVALID_ROOM"
+        })
+        return next()
+      }
+
+      const rMetadata = JSON.parse(room.metadata)
+
+      if(getCurrentTimestamp() < rMetadata.end_time) {
+        res.json({
+          success: false,
+          message: "INVALID_REQUEST"
+        })
+        return next()
+      }
+      
+      const currentNo = rMetadata.no == room.numParticipants ? 1 : rMetadata.no + 1
+      const participants = await getRoomParticipants(roomName)
+
+      const prevParticipant = participants?.find(p => {
+        const pMetadata = JSON.parse(p.metadata)
+        return pMetadata.no == rMetadata.no
+      })
+      const currentParticipant = participants?.find(p => {
+        const pMetadata = JSON.parse(p.metadata)
+        return pMetadata.no == currentNo
+      })
+
+      setRoomTurn(roomName, currentNo, getCurrentTimestamp())
+
+      if(prevParticipant) changeParticipantStatus(roomName, prevParticipant.identity, false)
+      if(currentParticipant) changeParticipantStatus(roomName, currentParticipant.identity, true)
+
+      res.json({
+        success: true,
+        data: room
+      })
+
+    } catch(err: any) {
+      let message = err.message?err.message:err
+      res.json({
+        success: false,
+        message: message
+      })
+    }
   })
-})
 
-router.post('/join', async (req: Request, res: Response, next): Promise<void> => {
+router.post('/skip-turn', 
+  body('room').isString().not().isEmpty().trim().escape(),
+  body('identity').isString().not().isEmpty().trim().escape(),
+  async (req: Request, res: Response, next): Promise<void> => {
 
-  const {room, participant}: { room: string, participant: string } = req.body
+    const errors = validationResult(req)
 
-  try {
+    if (!errors.isEmpty()) {
+      res.json({
+        success: false,
+        message: "INVALID_PARAMS"
+      })
+      return next()
+    }
 
-    const roomName = room
-    const participantName = participant
+    const {room: roomName, identity}: { room: string, identity: string } = req.body
 
-    const at = new AccessToken(undefined, undefined, {
-      identity: participantName,
-    })
+    try {
+      let room = await getRoom(roomName)
+      if(!room || room.numParticipants < 1) {
+        res.json({
+          success: false,
+          message: "INVALID_ROOM"
+        })
+        return next()
+      }
 
-    at.addGrant({ roomJoin: true, room: roomName });
+      let participant = await getParticipant(roomName, identity)
 
-    const token = at.toJwt()
-    
-    res.json({
-      success: true,
-      data: token
-    })
+      if(!participant) {
+        res.json({
+          success: false,
+          message: "INVALID_PARTICIPANT"
+        })
+        return next()
+      }
 
-  } catch(err: any) {
-    let message = err.message?err.message:err
-    res.json({
-      success: false,
-      message: message
-    })
+      const pMetadata = JSON.parse(participant.metadata)
+      const rMetadata = JSON.parse(room.metadata)
+
+      if(pMetadata.no != rMetadata.no) {
+        res.json({
+          success: false,
+          message: "INVALID_REQUEST"
+        })
+        return next()
+      }
+      
+      const currentNo = rMetadata.no == room.numParticipants ? 1 : rMetadata.no + 1
+      const participants = await getRoomParticipants(roomName)
+
+      const nextParticipant = participants?.find(p => {
+        const cMetadata = JSON.parse(p.metadata)
+        return cMetadata.no == currentNo
+      })
+
+      setRoomTurn(roomName, currentNo, getCurrentTimestamp())
+      changeParticipantStatus(roomName, participant.identity, false)
+      if(nextParticipant) changeParticipantStatus(roomName, nextParticipant.identity, true)
+
+      res.json({
+        success: true,
+        data: room
+      })
+
+    } catch(err: any) {
+      let message = err.message?err.message:err
+      res.json({
+        success: false,
+        message: message
+      })
+    }
+
+  })
+
+router.use('/webhook', express.raw())
+router.post('/webhook', async (req: Request, res: Response, next): Promise<void> => {
+  const event: WebhookEvent = receiver.receive(req.body, req.get('Authorization'))
+  switch (event.event) {
+    case 'room_started':
+      break
+    case 'room_finished':
+      break
+    case 'participant_joined':
+      const { room, participant } = event
+      if(!room || !participant) {
+        return
+      }
+      const no = room.numParticipants + 1
+      setParticipantNo(room.name, participant.identity, no)
+      setRoomTurn(room.name, no, getCurrentTimestamp())
+      break
+    case 'participant_left':
+      break
+    default:
+      break
   }
-})
 
-router.post('/create', async (req: Request, res: Response, next): Promise<void> => {
-
-  const {room: name}: { room: string } = req.body
-
-  try {
-    
-    const svc = new RoomServiceClient(livekitHost)
-
-    const opts = {
-      name: name,
-      emptyTimeout: 10 * 60, // timeout in seconds
-      maxParticipants: 5,
-    };
-
-    let room = await svc.createRoom(opts)
-    
-    res.json({
-      success: true,
-      data: room
-    })
-
-  } catch(err: any) {
-    let message = err.message?err.message:err
-    res.json({
-      success: false,
-      message: message
-    })
-  }
-})
-
-router.post('/mute', async (req: Request, res: Response, next): Promise<void> => {
-
-  const {room, identity}: { room: string, identity: string } = req.body
-
-  try {
-    
-    const svc = new RoomServiceClient(livekitHost)
-
-    let participantInfo = await svc.updateParticipant(room, identity, undefined, {
-      canSubscribe: true,
-      canPublish: false,
-      canPublishData: false,
-      hidden: false,
-      recorder: true
-    })
-    
-    res.json({
-      success: true,
-      data: participantInfo
-    })
-
-  } catch(err: any) {
-    let message = err.message?err.message:err
-    res.json({
-      success: false,
-      message: message
-    })
-  }
-})
-
-router.post('/unmute', async (req: Request, res: Response, next): Promise<void> => {
-
-  const {room, identity}: { room: string, identity: string } = req.body
-
-  try {
-    
-    const svc = new RoomServiceClient(livekitHost)
-
-    let participantInfo = await svc.updateParticipant(room, identity, undefined, {
-      canSubscribe: true,
-      canPublish: true,
-      canPublishData: true,
-      hidden: false,
-      recorder: true
-    })
-    
-    res.json({
-      success: true,
-      data: participantInfo
-    })
-
-  } catch(err: any) {
-    let message = err.message?err.message:err
-    res.json({
-      success: false,
-      message: message
-    })
-  }
-})
-
-router.post('/leave', async (req: Request, res: Response, next): Promise<void> => {
-
-  const {room, identity}: { room: string, identity: string } = req.body
-
-  try {
-    
-    
-
-  } catch(err: any) {
-    let message = err.message?err.message:err
-    res.json({
-      success: false,
-      message: message
-    })
-  }
-})
-
-router.post('/skip-turn', async (req: Request, res: Response, next): Promise<void> => {
-
-  const {room, identity}: { room: string, identity: string } = req.body
-
-  try {
-    
-    
-
-  } catch(err: any) {
-    let message = err.message?err.message:err
-    res.json({
-      success: false,
-      message: message
-    })
-  }
 })
 
 export default router
---
+++ src/utils/livekit.ts
@@ -0,0 +1,139 @@
+import { ParticipantInfo, Room, RoomServiceClient } from "livekit-server-sdk";
+import { host as livekitHost } from '../config/livekit'
+
+const svc = new RoomServiceClient(livekitHost)
+
+export const getRoom = async (name: string): Promise<Room | undefined> => {
+
+  const rooms = await getAllRooms()
+  if(rooms) {
+    return rooms.find(room => room.name === name) 
+  }
+  return
+}
+
+export const getAllRooms = async (): Promise<Room[] | undefined> => {
+  
+  return svc.listRooms().then((rooms: Room[]) => {
+    return rooms
+  }).catch(error => {
+    console.log(error.message?error.message:error)
+    return undefined
+  })
+
+}
+
+export const createRoom = async (name: string): Promise<Room> => {
+
+  const opts = {
+    name: name,
+    emptyTimeout: 10 * 60, // timeout in seconds
+    maxParticipants: 5,
+  }
+
+  return await svc.createRoom(opts)
+}
+
+export const getRoomParticipants = async (room: string): 
+  Promise<ParticipantInfo[] | undefined> => {
+    return svc.listParticipants(room)
+      .then(
+        (participants: ParticipantInfo[]) => {
+          return participants
+        }
+      )
+      .catch(error => {
+        console.log(error.message?error.message:error)
+        return undefined
+      })
+}
+
+export const getParticipant = async (room: string, identity: string): 
+  Promise<ParticipantInfo | undefined> => {
+    return svc.getParticipant(room, identity)
+      .then(
+        (participant: ParticipantInfo) => {
+          return participant
+        }
+      )
+      .catch(error => {
+        console.log(error.message?error.message:error)
+        return undefined
+      }) 
+  }
+
+export const setParticipantNo = 
+  async (room: string, identity: string, no: number) => {
+    const metadata = JSON.stringify({
+      no: no
+    })
+    return svc.updateParticipant(room, identity, metadata)
+      .then(
+        (participant: ParticipantInfo) => {
+          return participant
+        }
+      )
+      .catch(error => {
+        console.log(error.message?error.message:error)
+        return undefined
+      })
+  }
+
+export const extractLastParticipant = async (room: string): 
+  Promise<ParticipantInfo | undefined> => {
+    let participants = await getRoomParticipants(room)
+    let result = undefined
+    let lastNo = 0
+    if(participants) {
+      participants.map(p => {
+        const metadata = JSON.parse(p.metadata)
+        if(metadata.no > lastNo) {
+          result = p
+          lastNo = metadata.no
+        }
+      })
+    }
+    return result
+  }
+
+export const setRoomTurn = 
+  async (room:string, currentNo: number, startTime: number): 
+  Promise<Room | undefined> => {
+    const metadata = JSON.stringify({
+      no: currentNo,
+      start_time: startTime.toString(),
+      end_time: startTime + ( 1 * 60 )
+    })
+    return svc.updateRoomMetadata(room, metadata)
+      .then(
+        (room: Room) => {
+          return room
+        }
+      )
+      .catch(error => {
+        console.log(error.message?error.message:error)
+        return undefined
+      })
+  }
+
+export const changeParticipantStatus = 
+  async (room: string, identity: string, canPublish: boolean):
+  Promise<ParticipantInfo | undefined> => {
+    return svc.updateParticipant(room, identity, undefined, {
+      canSubscribe: true,
+      canPublish: canPublish,
+      canPublishData: false,
+      hidden: false,
+      recorder: true
+    })
+    .then(
+      (participant: ParticipantInfo) => {
+        return participant
+      }
+    )
+    .catch(error => {
+      console.log(error.message?error.message:error)
+      return undefined
+    })
+  }
+
